9e48b4d6b6a1bc887efebcc50d1b0be5be19f36c
unknown <seledkovti@gmail.com>
2022-02-25 02:41:02 +0300

    2 solution by find into dom

---
+++ 033_DOM_JS/app.js
@@ -0,0 +1,5 @@
+console.dir(window.document);
+
+// document.body.children[1].children[0].href = 'https://google.com';
+let acnhorElement = document.getElementById('external-link');
+acnhorElement.href = 'https://google.com';
