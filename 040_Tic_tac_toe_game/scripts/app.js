6189cff14b4901c7bfa8ca5e96f44a67387d8f21
unknown <seledkovti@gmail.com>
2022-03-11 21:59:11 +0300

    new keyword

--- 040_Tic_tac_toe_game/scripts/app.js
+++ 040_Tic_tac_toe_game/scripts/app.js
@@ -1,5 +1,6 @@
 const playerConfigOverlayElement = document.getElementById('config-overlay');
-const backdropOverlayElement = document.querySelector('.backdrop');
+const backdropElement = document.getElementById('backdrop');
+const formElement = document.querySelector('form');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
@@ -7,5 +8,6 @@ const cancelEditPlayerBtnElement = document.getElementById('cancel-edit-name-btn
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
 editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
-backdropOverlayElement.addEventListener('click', closePlayerConfig);
+backdropElement.addEventListener('click', closePlayerConfig);
 cancelEditPlayerBtnElement.addEventListener('click', closePlayerConfig);
+formElement.addEventListener('submit', savePlayerConfig);
--- 040_Tic_tac_toe_game/scripts/config.js
+++ 040_Tic_tac_toe_game/scripts/config.js
@@ -1,8 +1,15 @@
 function openPlayerConfig() {
   playerConfigOverlayElement.style.display = 'block';
-  backdropOverlayElement.style.display = 'block';
+  backdropElement.style.display = 'block';
 }
+
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = 'none';
-  backdropOverlayElement.style.display = 'none';
+  backdropElement.style.display = 'none';
+}
+
+function savePlayerConfig(e) {
+  e.preventDefault();
+  console.log(e);
+  // closePlayerConfig();
 }
