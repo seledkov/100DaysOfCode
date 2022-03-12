c066919e2c77925d91677d8f8f0897ce8217fdb2
unknown <seledkovti@gmail.com>
2022-03-12 23:44:40 +0300

    add popup logic validaton and error message

--- 040_Tic_tac_toe_game/scripts/app.js
+++ 040_Tic_tac_toe_game/scripts/app.js
@@ -1,6 +1,14 @@
+let editedPlayer = 0;
+
+const players = [
+  { name: '', symbol: 'X' },
+  { name: '', symbol: 'O' },
+];
+
 const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
+const errorOutputElement = document.getElementById('error-output');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
--- 040_Tic_tac_toe_game/scripts/config.js
+++ 040_Tic_tac_toe_game/scripts/config.js
@@ -1,4 +1,5 @@
-function openPlayerConfig() {
+function openPlayerConfig(e) {
+  editedPlayer = +e.target.dataset.playerid; // all dataset = string[]
   playerConfigOverlayElement.style.display = 'block';
   backdropElement.style.display = 'block';
 }
@@ -6,10 +7,18 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = 'none';
   backdropElement.style.display = 'none';
+  formElement.firstElementChild.classList.remove('error');
+  errorOutputElement.textContent = '';
 }
 
 function savePlayerConfig(e) {
   e.preventDefault();
   console.log(e);
-  // closePlayerConfig();
+  const formData = new FormData(event.target);
+  const enteredlayerName = formData.get('playerName').trim();
+  if (!enteredlayerName) {
+    event.target.firstElementChild.classList.add('error');
+    errorOutputElement.textContent = 'Please enter a valid name!';
+    return; // stop function
+  }
 }
