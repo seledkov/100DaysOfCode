0dd880cc765b8d83e6418fb6170000b2469227b7
unknown <seledkovti@gmail.com>
2022-03-12 23:58:21 +0300

    lession 42  data attribute, form clear and update name

--- 040_Tic_tac_toe_game/scripts/config.js
+++ 040_Tic_tac_toe_game/scripts/config.js
@@ -9,16 +9,24 @@ function closePlayerConfig() {
   backdropElement.style.display = 'none';
   formElement.firstElementChild.classList.remove('error');
   errorOutputElement.textContent = '';
+  document.getElementById('playerName').value = ''; // clear input data
 }
 
 function savePlayerConfig(e) {
   e.preventDefault();
   console.log(e);
   const formData = new FormData(event.target);
-  const enteredlayerName = formData.get('playerName').trim();
-  if (!enteredlayerName) {
+  const enteredPlayerName = formData.get('playerName').trim();
+  if (!enteredPlayerName) {
+    // maybe add check to dublicate name if need
     event.target.firstElementChild.classList.add('error');
     errorOutputElement.textContent = 'Please enter a valid name!';
     return; // stop function
   }
+
+  const updatedPlayer = document.querySelector('#player-' + editedPlayer + '-name');
+  updatedPlayer.textContent = enteredPlayerName;
+  players[editedPlayer - 1].name = enteredPlayerName;
+
+  closePlayerConfig();
 }
