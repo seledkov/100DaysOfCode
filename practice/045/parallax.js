889160f81b4f11f0227e600a5591795906b7b81f
unknown <seledkovti@gmail.com>
2022-04-01 21:24:15 +0300

    see promise

--- practice/045/parallax.js
+++ practice/045/parallax.js
@@ -1,5 +1,5 @@
 const imageElement = document.getElementById('parallax-image');
 new simpleParallax(imageElement, {
   scale: 1.6,
-  orientation: '	up - right - down',
+  // orientation: '	up - right - down',
 });
--- practice/047_nodeJs/app.js
+++ practice/047_nodeJs/app.js
@@ -1,4 +1,13 @@
 const userName = 'Max';
-
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log('first item');
+  }, 4000);
+});
 // use: node app.js   in terminal
 console.log(userName);
+promise.then(
+  setTimeout(() => {
+    console.log('secod');
+  }, 2000),
+);
