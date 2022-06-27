020ad8e24079000c61b46a566eceb1d6ed4a50ab
unknown <seledkovti@gmail.com>
2022-06-28 01:07:34 +0300

    end todo list, basic example

--- 099_Basic_Vue/scripts/app.js
+++ 099_Basic_Vue/scripts/app.js
@@ -1,14 +1,52 @@
 const TodosApp = {
   data() {
-    return { newTodo: 'first todo', enteredTodoText: '' };
+    return {
+      todos: [],
+      enteredTodoText: '',
+      editedTodoId: null,
+    };
   },
   methods: {
     saveTodo(event) {
       event.preventDefault();
 
       //this is possible because the 'this' keyword is actually kind of tricky in javascript. it can be bound to all kinds of things and here vue binds in to the object returned inside of 'data()'
-      this.newTodo = this.enteredTodoText;
+      // this.newTodo = this.enteredTodoText;
+
+      if (this.editedTodoId) {
+        // upd
+        const todoId = this.editedTodoId;
+        const todoIndex = this.todos.findIndex(function (item) {
+          return item.id === todoId;
+        });
+        const updatedTodo = {
+          id: this.todos[todoIndex].id,
+          text: this.enteredTodoText,
+        };
+
+        this.todos[todoIndex] = updatedTodo;
+        this.editedTodoId = null;
+      } else {
+        const newTodo = {
+          text: this.enteredTodoText,
+          id: Math.random(),
+        };
+        this.todos.push(newTodo);
+      }
+
       this.enteredTodoText = '';
+    },
+    startEditTodo(todoId) {
+      this.editedTodoId = todoId;
+      const todo = this.todos.find(function (item) {
+        return item.id === todoId;
+      });
+      this.enteredTodoText = todo.text;
+    },
+    deleteTodo(todoId) {
+      this.todos = this.todos.filter(function (item) {
+        return item.id !== todoId;
+      });
     },
   },
 };
